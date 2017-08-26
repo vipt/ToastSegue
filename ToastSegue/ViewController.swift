89b7a5f81e7d985473a121061c3eced8abf5369a
motoki <motoki@takahashi-no-macbook-air.local>
2017-08-26 17:54:20 +0900

    Initial Commit

---
+++ ToastSegue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToastSegue
+//
+//  Created by motoki on 2017/08/26.
+//  Copyright © 2017年 vipt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
